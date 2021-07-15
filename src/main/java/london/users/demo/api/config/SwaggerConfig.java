8b6bc1c5469b03980ce324464a8c6b440d9a963e
Paris Tsantarliotis <ptsantar@users.noreply.github.com>
2021-07-15 12:09:44 +0100

    Update swagger configuration.

--- src/main/java/london/users/demo/api/config/SwaggerConfig.java
+++ src/main/java/london/users/demo/api/config/SwaggerConfig.java
@@ -1,29 +1,51 @@
 package london.users.demo.api.config;
 
-import org.springframework.util.StringUtils;
+import london.users.demo.api.model.ApiResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
+import static springfox.documentation.builders.PathSelectors.any;
+@Configuration
 public class SwaggerConfig implements WebMvcConfigurer {
-    private final String baseUrl;
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("london.users.demo.api"))
+                .paths(any())
+                .build()
+                .apiInfo(metaData())
+                .pathMapping("/");
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("London Users API")
+                .description("API which calls a 3rd-party API at https://bpdts-test-app.herokuapp.com/, " +
+                        "and returns people who are listed as either living in a specified city (e.g. London), or whose " +
+                        "current coordinates are within 50 miles of it.")
+                .version("1.0")
+                .contact(new Contact(
+                        "Paraskevas Tsantarliotis",
+                        "https://github.com/ptsantar/",
+                        "-"))
 
-    public SwaggerConfig(String baseUrl) {
-        this.baseUrl = baseUrl;
+                .build();
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String baseUrl = StringUtils.trimTrailingCharacter(this.baseUrl, '/');
-        registry.
-                addResourceHandler(baseUrl + "/swagger-ui/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
-                .resourceChain(false);
-    }
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController(baseUrl + "/swagger-ui/")
-                .setViewName("forward:" + baseUrl + "/swagger-ui/index.html");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
